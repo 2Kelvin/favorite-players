88b564f8728831296dd5f949fb015f5185030ad8
2Kelvin <rocketmancodes02@gmail.com>
2024-05-02 11:58:38 +0300

    done with adding playerCards business logic
    I can now move to the next PlayerCard through clicking the buttons

--- app/src/main/java/com/example/favoriteplayers/MainActivity.kt
+++ app/src/main/java/com/example/favoriteplayers/MainActivity.kt
@@ -24,6 +24,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -53,58 +57,91 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Player object structure
+data class Player(
+    val imageId: Int,
+    val name: String,
+    val jerseyNumber: Int,
+    val position: String,
+    val team: String,
+)
+
+// list of all my players
+val myListOfPlayers = listOf(
+    Player( // curry
+        imageId = R.drawable.crry,
+        name = "Stephen Curry",
+        jerseyNumber = 30,
+        position = "Point Guard",
+        team = "Golden State Warriors"
+    ),
+    Player( // kawhi
+        imageId = R.drawable.kawhi,
+        name = "Kawhi Leonard",
+        jerseyNumber = 2,
+        position = "Small Forward",
+        team = "Los Angeles Clippers"
+    ),
+    Player( // ronaldo
+        imageId = R.drawable.roro,
+        name = "Christiano Ronaldo",
+        jerseyNumber = 7,
+        position = "Striker / Left Winger",
+        team = "Al Nassir"
+    ),
+)
+
 @Composable
 fun FavoritePlayersApp(modifier: Modifier = Modifier) {
+    // state
+    var arrId by remember { mutableStateOf(0) }
+    val cannotGoPrev = arrId <= 0
+    val cannotGoNext = arrId >= myListOfPlayers.size - 1
+
     Column(
         modifier = modifier
-            .padding(top = 80.dp, bottom = 15.dp, start = 15.dp, end = 15.dp)
+            .padding(top = 100.dp, bottom = 20.dp, start = 20.dp, end = 20.dp)
             .fillMaxSize()
     ) {
         Column(modifier = modifier.weight(1f)) {
-            // reusable PlayerCard here
-            PlayerCard()
+            // reusable PlayerCard
+            // access the player using their index in the array
+            PlayerCard(myListOfPlayers[arrId])
         }
 
         // buttons
         Row(
             modifier = modifier
                 .fillMaxWidth()
-                .height(100.dp),
+                .height(120.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            ElevatedButton(
-                onClick = { /*TODO*/ },
-                modifier = modifier.weight(1f),
-                // set button background-color
-                colors = ButtonDefaults.buttonColors(Color.Black),
-                // set custom button border-radius
-                shape = RoundedCornerShape(10.dp)
-            ) {
-                Text(
-                    text = "Previous"
-                )
-            }
+            ReusableElevatedButton(
+                btnText = "Previous",
+                // move to the previous array index if the index is not 0 (the first player)
+                handleClick = { if (arrId > 0) arrId-- },
+                isDisabled = cannotGoPrev,
+                modifier = modifier.weight(1f)
+            )
 
             Spacer(modifier = modifier.width(50.dp))
 
-            ElevatedButton(
-                onClick = { /*TODO*/ },
-                modifier = modifier.weight(1f),
-                // set button background-color
-                colors = ButtonDefaults.buttonColors(Color.Black),
-                // set custom button border-radius
-                shape = RoundedCornerShape(10.dp)
-            ) {
-                Text(
-                    text = "Next"
-                )
-            }
+            ReusableElevatedButton(
+                btnText = "Next",
+                // move to the next array index if not in the array's last item index or higher
+                handleClick = { if (arrId < myListOfPlayers.size - 1) arrId++ },
+                isDisabled = cannotGoNext,
+                modifier = modifier.weight(1f)
+            )
         }
     }
 }
 
 @Composable
-fun PlayerCard(modifier: Modifier = Modifier) {
+fun PlayerCard(
+    player: Player,
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier
             // take all the weight(1f) height of the screen
@@ -122,37 +159,54 @@ fun PlayerCard(modifier: Modifier = Modifier) {
             )
         ) {
             Image(
-                painter = painterResource(id = R.drawable.curry),
-                contentDescription = "player",
+                painter = painterResource(id = player.imageId),
+                contentDescription = player.name,
                 // make the image fill the 1f weight height
                 contentScale = ContentScale.FillHeight,
                 modifier = modifier
                     .weight(1f)
                     .fillMaxHeight()
-                    .padding(30.dp)
+                    .padding(40.dp)
                     // custom Image border-radius
                     .clip(shape = RoundedCornerShape(3.dp))
             )
         }
 
-        Spacer(modifier = modifier.height(50.dp))
+        Spacer(modifier = modifier.height(40.dp))
 
         Column(
             modifier = modifier
                 .fillMaxWidth()
                 .background(Color(0xFFededf1))
-                .padding(15.dp),
+                .padding(horizontal = 15.dp, vertical = 25.dp),
             verticalArrangement = Arrangement.Center
         ) {
-            Text(text = "Steph Curry (30)", fontSize = 20.sp)
+            Text(text = player.name + " (${player.jerseyNumber})", fontSize = 20.sp)
             Text(
-                text = "Point Guard",
+                text = player.position,
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Light,
                 color = Color.Gray,
             )
-            Text(text = "Golden State Warriors", fontSize = 16.sp)
+            Text(text = player.team, fontSize = 16.sp)
         }
+    }
+}
+
+// reusable elevated button
+@Composable
+fun ReusableElevatedButton(btnText: String, handleClick: () -> Unit, isDisabled: Boolean, modifier: Modifier = Modifier) {
+    ElevatedButton(
+        onClick = handleClick,
+        modifier = modifier.height(50.dp),
+        // set button background-color
+        colors = ButtonDefaults.buttonColors(Color.Black),
+        // set custom button border-radius
+        shape = RoundedCornerShape(10.dp),
+        // if isDisabled expression is true, passing false to enabled to disable button
+        enabled = !isDisabled,
+    ) {
+        Text(text = btnText, fontSize = 16.sp)
     }
 }
 
